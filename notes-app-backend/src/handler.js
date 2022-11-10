73bc1f172693ec9b401611252003551e2aa06d6f
Fnwrkio <firmansyah.assignment@gmail.com>
2022-11-10 20:01:16 +0700

    module: menampilkan catatan

--- notes-app-backend/src/handler.js
+++ notes-app-backend/src/handler.js
@@ -38,4 +38,34 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotes = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteById = (request, h) => {
+  const { id } = request.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNoteHandler, getAllNotes, getNoteById };
--- notes-app-backend/src/routes.js
+++ notes-app-backend/src/routes.js
@@ -1,6 +1,16 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotes, getNoteById } = require('./handler');
 
 const routes = [
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotes,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteById,
+  },
   {
     method: 'POST',
     path: '/notes',
